3cf7226ebd79d1fea3406c721e7630e1877bdfcc
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-19 02:37:33 +0530

    JavaScript

---
+++ Basic/var_let_const.js
@@ -0,0 +1,26 @@
+console.log("var, let and const")
+var a = 45
+var b = "ajlal"
+var c = true
+var d = null
+var e = undefined
+const author = "Mohd Ajlal"
+//let author = "abc" : Throws an error because constant can't be changed 
+//author = "abc" : Throws an error because constant can't be changed
+
+{
+    var b = "mohd"
+    console.log(b)
+}
+console.log(b)
+
+let b2 = "ajlal"
+// let b2 = "ajlal" // we can't redeclare let variable
+// b = "ajlal" // we can change value of variable
+{
+    let b2 = "mohd"
+    console.log(b2)
+}
+console.log(b2)
+
+// we can't use let and var with same name
---
+++ Basic/variable.js
@@ -0,0 +1,6 @@
+// javascript allows you to change the datatype at runtime. 
+// var a = 57
+let a = 57 
+console.log(a)
+a = "ajlal"
+console.log(a)
