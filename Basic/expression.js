e962a33e1fa9992d73172fd201f34a86da45abc5
Mohd Ajlal <ajlal00786@gmail.com>
2023-02-26 18:45:04 +0530

    Js

--- Basic/expression.js
+++ Basic/expression.js
@@ -7,6 +7,15 @@ console.log("Multiplication: 2*3 = ", 2*3)
 console.log("Division: 2/3 = ", 2/3)
 console.log("Modulus: 2%3 = ", 2%3)
 console.log("Exponentiation: 2**3 = ", 2**3)
+// increment and decrement operators
+let x = 2
+console.log("x = ", x)
+console.log("Increment: x++ = ", x++)
+console.log("Increment: ++x = ", ++x)
+console.log("Decrement: x-- = ", x--)
+console.log("Decrement: --x = ", --x)
+
+console.log()
 
 console.log("Assignment Operators")
 console.log("Assignment: a = 2")
@@ -24,6 +33,8 @@ a %= 3
 console.log("Exponentiation Assignment: a **= 3")
 a **= 3
 
+console.log()
+
 console.log("Comparison Operators")
 console.log("Equal: 2 == 3", 2 == 3)
 console.log("Not Equal: 2 != 3", 2 != 3)
@@ -32,10 +43,14 @@ console.log("Less Than: 2 < 3", 2 < 3)
 console.log("Greater Than or Equal: 2 >= 3", 2 >= 3)
 console.log("Less Than or Equal: 2 <= 3", 2 <= 3)
 
+console.log()
+
 console.log("Logical Operators")
 console.log("Logical AND: true && true", true && true)
 console.log("Logical OR: true || false", true || false)
 console.log("Logical NOT: !true", !true)
+
+console.log()
 
 console.log("Bitwise Operators")
 console.log("Bitwise AND: 2 & 3", 2 & 3)
@@ -46,6 +61,8 @@ console.log("Bitwise Left Shift: 2 << 3", 2 << 3)
 console.log("Bitwise Right Shift: 2 >> 3", 2 >> 3)
 console.log("Bitwise Zero Fill Right Shift: 2 >>> 3", 2 >>> 3)
 
+console.log()
+
 console.log("String Operators")
 console.log("Concatenation: 'Hello' + 'World'", 'Hello' + 'World')
 let str = "Hello";
@@ -55,15 +72,23 @@ str += "World";
 // console.log(str);
 console.log("Append: 'Hello' += 'World'", str)
 
+console.log()
+
 console.log("Conditional (Ternary) Operator")
 console.log("Ternary: 2 > 3 ? 'Yes' : 'No'", 2 > 3 ? 'Yes' : 'No')
+
+console.log()
 
 console.log("Comma Operator")
 console.log("Comma: (1, 2, 3)", (1, 2, 3))
 
+console.log()
+
 console.log("Operator Precedence")
 console.log("Precedence: 2 + 3 * 4", 2 + 3 * 4)
 console.log("Precedence: (2 + 3) * 4", (2 + 3) * 4)
+
+console.log()
 
 console.log("Operator Associativity")
 console.log("Associativity: 2 + 3 + 4", 2 + 3 + 4)
@@ -72,6 +97,8 @@ console.log("Associativity: 2 * 3 * 4", 2 * 3 * 4)
 console.log("Associativity: 2 / 3 / 4", 2 / 3 / 4)
 console.log("Associativity: 2 % 3 % 4", 2 % 3 % 4)
 console.log("Associativity: 2 ** 3 ** 4", 2 ** 3 ** 4)
+
+console.log()
 
 console.log("Operator Precedence Table")
 console.log("Precedence Table: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence")
