655ab9ededb14a62b144242ef3ec4f057e533565
Mohd Ajlal <ajlal00786@gmail.com>
2023-08-23 02:34:35 +0530

    JS

--- type_conversion/script.js
+++ type_conversion/script.js
@@ -1,2 +1,43 @@
-let type = "Ajlal";
+// task 1
+
+let type = 22;
 alert(typeof type);
+
+
+//  Task 2
+// automatillay
+let num = 5 + null;
+document.write(num + "<br>");
+document.write(typeof num + "<br>");
+
+// Task 3
+
+let str = "5" + 2;
+document.write(str + "<br>");
+document.write(typeof str + "<br>");
+
+// Task 4
+
+let num1 = 5 * "2";
+document.write(num1 + "<br>");
+document.write(typeof num1 + "<br>");   
+
+
+// Task 5
+
+let num2 = "5" * "Hello";
+document.write(num2 + "<br>");
+document.write(typeof num2 + "<br>");
+
+// Manual Type Conversion
+
+// 1. String()
+// 2. Number()
+// 3. Boolean()
+
+// Task 6
+
+let str1 = "123";
+let number = 10;
+let out = Number(str1);
+document.write(out + number + "<br>");
