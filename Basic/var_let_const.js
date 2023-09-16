990de78554fd7ef4aec2a0c28a7801619e57e75a
Mohd Ajlal <ajlal00786@gmail.com>
2023-09-17 03:01:28 +0530

    JS

--- Basic/var_let_const.js
+++ Basic/var_let_const.js
@@ -1,31 +1,32 @@
-console.log("var, let and const")
-var a = 45
-var b = "ajlal"
-var c = true
-var d = null
-var e = undefined
-const author = "Mohd Ajlal"
-//let author = "abc" : Throws an error because constant can't be changed 
+console.log("var, let and const");
+var a = 45;
+var b = "ajlal";
+var c = true;
+var d = null;
+var e = undefined;
+const author = "Mohd Ajlal";
+//let author = "abc" : Throws an error because constant can't be changed
 //author = "abc" : Throws an error because constant can't be changed
 
 {
-    var b = "mohd"
-    console.log(b)
+  var b = "mohd";
+  console.log(b);
 }
-console.log(b)
+console.log(b);
 
-let b2 = "ajlal"
+let b2 = "ajlal";
 // let b2 = "ajlal" // we can't redeclare let variable
 // b = "ajlal" // we can change value of variable
 {
-    let b2 = "mohd"
-    console.log(b2)
+  let b2 = "mohd";
+  console.log(b2);
 }
-console.log(b2)
+console.log(b2);
 
 // we can't use let and var with same name
-
 
 // var => function scope
 // let => block scope
 // const => block scope
+
+console.table([a, b2]);
