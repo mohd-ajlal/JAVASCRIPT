7cdc387c60db67093cf108fe07e6ec9b4bca7a92
Mohd Ajlal <ajlal00786@gmail.com>
2023-09-18 02:54:01 +0530

    JS

---
+++ String-Manipulation/string.js
@@ -0,0 +1,13 @@
+const st = new String("Ajlal");
+
+console.log(st);
+
+console.log(st.__proto__);
+
+console.log(st.length)
+console.log(st.toLowerCase())
+console.log(st.toUpperCase())
+
+console.log(st);
+console.log(st.charAt(3))
+console.log(st.indexOf("A"))
