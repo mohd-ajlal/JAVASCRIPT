e851cb89791472745aeef2f48f9461f78e6ea1c3
Mohd Ajlal <ajlal00786@gmail.com>
2023-11-18 03:18:20 +0530

    JS

--- DOM/dom.js
+++ DOM/dom.js
@@ -5,3 +5,13 @@
 // console.log(document)
 
 // console.dir(document)
+
+// in console log
+
+// document.getElementById('id')
+// document.getElementsByClassName('class')
+// document.getElementsByTagName('tag')
+// document.querySelector('selector')
+// document.querySelectorAll('selector')
+
+// document.getElementById('id').innerHTML = 'new text'
